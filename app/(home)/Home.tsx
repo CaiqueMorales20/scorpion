3480e908bbb8ce2d757895f62655fcfd95a9f6f9
Caique <caiquemorales20@gmail.com>
2023-05-20 17:50:11 -0300

    :construction: Firebase

--- app/(home)/Home.tsx
+++ app/(home)/Home.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import database from "@/firebase/config";
+
 // Styles
 import styles from "./Home.module.css";
 
 // Functional Component
 export default async function Home() {
+	useEffect(() => {
+		// Exemplo de leitura de dados do Realtime Database
+		console.log(database);
+	}, []);
+
 	// Rendering
 	return (
 		<div className={styles.home}>
